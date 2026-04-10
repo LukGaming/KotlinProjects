f63867f22e84d7e480d86aeafae9801796de0936
LukGaming <thelukgaming333@gmail.com>
2026-04-10 13:07:32 -0300

    Dia 09: Criando Activity

--- DummyLogin/app/src/main/java/com/example/dummylogin/features/auth/presentation/LoginActivity.kt
+++ DummyLogin/app/src/main/java/com/example/dummylogin/features/auth/presentation/LoginActivity.kt
@@ -1,7 +1,34 @@
 package com.example.dummylogin.features.auth.presentation
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.example.dummylogin.R
 
 class LoginActivity: AppCompatActivity() {
 
+
+    lateinit var etEmail: EditText
+    lateinit var etPassword: EditText
+
+    lateinit var btnLogin: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.login_activity)
+
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+
+        }
+
+
+
+    }
 }
