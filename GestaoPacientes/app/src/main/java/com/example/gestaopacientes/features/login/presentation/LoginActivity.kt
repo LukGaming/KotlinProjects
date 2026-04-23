18eea3b0fbef897ac049767669e3665e5e332edc
LukGaming <thelukgaming333@gmail.com>
2026-04-23 08:25:06 -0300

    Dia 21: Inicializando App de Gestão de Pacientes

---
+++ GestaoPacientes/app/src/main/java/com/example/gestaopacientes/features/login/presentation/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.gestaopacientes.features.login.presentation
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gestaopacientes.R
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login)
+    }
+}
